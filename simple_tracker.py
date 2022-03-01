5a01e26bfa118cb6e67a60fe8c852728902d9047
Alex Day <adday@clemson.edu>
2022-03-01 12:25:06 -0500

    Make tracking script nicer

--- simple_tracker.py
+++ simple_tracker.py
@@ -1,10 +1,18 @@
+#! /usr/bin/env python
+
+"""Simple tracking solution based off triad_openvr
+
+Track the pose and velocity of devices connected to a steamVR system at some user-defined (default 250hz) frequency.
+"""
+
 import triad_openvr
 import time
 import sys
 
+# Connect to the VR system
 v = triad_openvr.triad_openvr()
-# v.print_discovered_objects()
 
+# Set the frequency of tracking
 if len(sys.argv) == 1:
     interval = 1/250
 elif len(sys.argv) == 2:
@@ -13,13 +21,30 @@ else:
     print("Invalid number of arguments")
     exit(1)
 
+# Define all controllers/trackers that we want to track. If the device
+# is not connected then it will not show up in the output
 to_track = ["controller_1", "tracker_1", "tracker_2", "tracker_3"]
+
+# Print the header
 print("device, x, y, z, dx, dy, dz, t")
+
+# Collect the start time so the tracked pose has a relative timestamp
 abs_start = time.time()
-while(True):
+
+# Loop forever, or at least until ctrl-c
+while True:
+    # Calculate the loop start time so that we only sleep the remaining
+    # time left in the loop after calculation
     loop_start = time.time()
 
+    # Capture each device
     for device in to_track:
+        # If the device is not connected then just continue
+        if device not in v.devices:
+            continue
+
+        # Capture the position (x, y, z) and velocity (dx, dy, dz) for each
+        # device, the orientation does not matter
         txt = device + ", "
         for each in v.devices[device].get_pose_euler()[:3]:
             txt += "%.4f" % each
@@ -27,6 +52,8 @@ while(True):
         for each in v.devices[device].get_velocity():
             txt += "%.4f" % each
             txt += ", "
+
+        # Add the relative timestamp
         txt += str(time.time() - abs_start)
 
         # Print out the data
